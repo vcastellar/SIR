2d7170745c0426721167c5f890645befb4905b69
vcastellar <vic.sebastia@gmail.com>
2025-12-12 11:41:49 +0100

    Refactor SIR analysis script

--- R/modelo.R
+++ R/modelo.R
@@ -1,163 +1,161 @@
+# Analisis del modelo SIR para COVID-19 en España
+# El script descarga las series de casos confirmados desde el repositorio
+# de la Universidad Johns Hopkins, calcula métricas de crecimiento y ajusta
+# un modelo SIR y uno logístico simples.
 
-library(dplyr)
-library(lubridate)
-library(tidyr)
-library(reshape2)
-library(xts)
-deaths <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
-                   stringsAsFactors = FALSE)
-head(deaths)
-deaths$Lat <- NULL
-deaths$Long <- NULL
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(lubridate)
+  library(tidyr)
+  library(reshape2)
+  library(xts)
+  library(zoo)
+  library(deSolve)
+})
 
+DATA_URL <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
-deathsLong <- melt(deaths, id.vars = c("Province.State", "Country.Region"))
-deathsLong$variable <- gsub("^X", "", deathsLong$variable)
-deathsLong$variable <- gsub("\\.", "-", deathsLong$variable)
-deathsLong$variable <- gsub("-20$", "-2020", deathsLong$variable)
+# Carga los datos de casos confirmados y elimina las columnas irrelevantes.
+load_case_data <- function(url = DATA_URL) {
+  read.csv(url, stringsAsFactors = FALSE) %>%
+    select(-Lat, -Long)
+}
 
-deathsLong$variable <- as.Date(as.character(deathsLong$variable), format = "%m-%d-%Y")
+# Convierte la tabla en formato largo y devuelve la serie del país solicitado.
+prepare_country_series <- function(data, country = "Spain", min_cases = 100) {
+  long <- melt(data, id.vars = c("Province.State", "Country.Region")) %>%
+    mutate(
+      variable = gsub("^X", "", variable),
+      variable = gsub("\\.", "-", variable),
+      variable = gsub("-20$", "-2020", variable),
+      date = as.Date(as.character(variable), format = "%m-%d-%Y")
+    )
 
-agregados  <- deathsLong %>% 
-  
-  group_by(Country.Region, variable) %>% 
-  summarise(deaths = sum(value)) %>% 
-  as.data.frame
+  country_ts <- long %>%
+    filter(Country.Region == country) %>%
+    arrange(date) %>%
+    transmute(date, cases = value) %>%
+    filter(cases > min_cases)
 
-sort(table(agregados$Country.Region))
+  if (nrow(country_ts) == 0) {
+    stop(paste0("No hay datos por encima de ", min_cases, " casos para ", country, "."))
+  }
 
-n_init_infect <- 100
+  list(
+    series = country_ts,
+    start_date = country_ts$date[1],
+    infected = round(country_ts$cases)
+  )
+}
 
-kk <- agregados %>% group_by(Country.Region) %>% 
-  filter(deaths > n_init_infect) %>% 
-  summarise(alpha = sum(diff(log(tail(deaths, 4))), na.rm = TRUE)/3) %>% data.frame() %>% 
-  mutate(z = log(2) / log(1 + alpha)) %>% 
-  arrange(z)
+# Calcula métricas de crecimiento: porcentaje diario y días en que se duplica.
+compute_growth_metrics <- function(infected) {
+  inc_pct <- rollapply(infected, width = 2, FUN = function(x) (x[2] - x[1]) / x[1], align = "left")
+  alpha <- rollapply(inc_pct, width = 4, FUN = mean)
+  doubling_days <- log(2) / log(1 + alpha)
+  doubling_days[is.infinite(doubling_days)] <- NA
+  list(inc_pct = inc_pct, doubling_days = doubling_days)
+}
 
-
-espanya <- deathsLong %>% filter(Country.Region == "Spain")
-N <- 47e6
-
-Infected <- round(espanya$value)
-Infected <- Infected[Infected > n_init_infect]
-
-
-Fecha <- as.Date(espanya$variable[which(espanya$value > n_init_infect)[1]])
-Fechas_Obs <- seq.Date(from = Fecha, length.out = length(Infected), by = "day")
-
-serie <- ts(Infected, start = c(2020, as.numeric(format(Fechas_Obs[1], "%j"))),
-            frequency = 365)
-plot(serie)
-
-serie <- xts(x = Infected, order.by = Fechas_Obs)
-plot(serie)
-plot(log2(serie))
-as.numeric(diff(log(serie)))
-plot(diff(log(serie)))
-
-IncPorc <- rollapply(Infected, width = 2, FUN = function(x) (x[2] - x[1])/x[1], align = "left")
-alpha <- rollapply(IncPorc, width = 4, FUN = mean)
-DiasDobla <- log(2) / (log(1 + alpha))
-serieDobla <- xts(x = DiasDobla, order.by = Fechas_Obs[-(1:4)])
-serieDobla[is.infinite(serieDobla)] <- NA
-plot(serieDobla, main = "nº de días estimados en lo que se dobla el nº de infectados", ylim = c(0, 1e3))
-# diás estimados en los que se doblan el número de infectados
-log(2) / log(1 + IncPorc)
-barplot(diff(serie), las = 2, cex.names = 0.7)
-
-# Infected <- c(45, 62, 121, 198, 291, 440, 571, 830, 1287, 1975, 2744, 4515, 5974, 7711, 9692, 11791, 14380, 17205, 20440)
-
-# - S: número de susceptibles
-# - I: número de infectados
-# - R: número de recobrados
-Day <- 1:(length(Infected))
-SIR <- function(time, state, parameters) {
+# Función que define el sistema de ecuaciones SIR.
+SIR <- function(time, state, parameters, N) {
   par <- as.list(c(state, parameters))
   with(par, {
-    dS <- -beta/N * I * S
-    dI <- beta/N * I * S - gamma * I
+    dS <- -beta / N * I * S
+    dI <- beta / N * I * S - gamma * I
     dR <- gamma * I
     list(c(dS, dI, dR))
   })
 }
 
+# Ajusta el modelo SIR mediante mínimos cuadrados.
+fit_sir_model <- function(infected, population, horizon_days) {
+  Day <- seq_along(infected)
+  init <- c(S = population - infected[1], I = infected[1], R = 0)
 
+  rss <- function(parameters) {
+    names(parameters) <- c("beta", "gamma")
+    out <- ode(y = init, times = Day, func = function(time, state, parameters) {
+      SIR(time, state, parameters, population)
+    }, parms = parameters)
+    fit <- out[, 3]
+    mean((infected - fit) ^ 2)
+  }
 
+  opt <- optim(c(0.5, 0.5), rss, method = "Nelder-Mead", control = list(maxit = 500, factr = 1e4))
+  opt_par <- setNames(opt$par, c("beta", "gamma"))
 
-library(deSolve)
-init <- c(S = N-Infected[1], I = Infected[1], R = 0)
-RSS <- function(parameters) {
-  names(parameters) <- c("beta", "gamma")
-  out <- ode(y = init, 
-             times = Day, 
-             func = SIR, 
-             parms = parameters)
-  fit <- out[ , 3]
-  mean((Infected - fit)^2)
+  t <- 1:(horizon_days + length(infected))
+  fit <- data.frame(ode(y = init, times = t, func = function(time, state, parameters) {
+    SIR(time, state, parameters, population)
+  }, parms = opt_par))
+
+  list(parameters = opt_par, fit = fit)
 }
 
-# optimize with some sensible conditions
-Opt <- optim(c(0.5, 0.5), 
-             RSS, 
-             method = "Nelder-Mead",
-             control = list(maxit = 500, factr = 1e4)) 
-Opt$message
-## [1] "CONVERGENCE: REL_REDUCTION_OF_F <= FACTR*EPSMCH"
+# Ajusta una curva logística simple por mínimos cuadrados.
+fit_logistic_curve <- function(infected, dates) {
+  Day <- seq_along(infected)
+  logistic <- function(parms, x) {
+    parms[3] / (1 + exp(-(x - parms[2]) / parms[1]))
+  }
 
-Opt_par <- setNames(Opt$par, c("beta", "gamma"))
-Opt_par
-##      beta     gamma 
-## 0.6746089 0.3253912
-## R0
-Opt_par[1] / Opt_par[2]
+  rss <- function(parms) {
+    out <- logistic(parms, x = Day)
+    sum((infected - out) ^ 2)
+  }
 
-t <- 1:(90 + length(Infected)) # time in days
-fit <- data.frame(ode(y = init, times = t, func = SIR, parms = Opt_par))
-
-
-
-Fechas <- seq.Date(from = Fecha, length.out = nrow(fit), by = "day")
-fit$time <- Fechas
-
-
-plot(fit$time, fit$S, type = "l", lwd = 2, ylim = c(0, N))
-lines(fit$time, fit$I, type = "l", lwd = 2, col = "red")
-lines(fit$time, fit$R, type = "l", lwd = 2, col = "green")
-points(Fechas[1:length(Infected)], Infected)
-grid()
-legend("left", c("Susceptibles", "Infectados", "Recobrados"), lty = 1, lwd = 2, col = 1:3, inset = 0.05)
-
-plot(fit$time, fit$I, type = "l", lwd = 2, col = "red", pch = 19)
-points(Fechas[1:length(Infected)], Infected)
-grid()
-
-
-
-logistic <- function(parms, x) {
-  parms[3]/(1 + exp(- (x - parms[2]) / parms[1]))
+  opt <- optim(c(1, 1, max(infected) * 2), rss, method = "L-BFGS-B")
+  fit <- logistic(opt$par, 1:length(dates))
+  list(parameters = opt$par, fit = fit)
 }
 
-parms <- c(A = 1, B = 1, C = 2e6)
-RSS <- function(parms) {
-  out <- logistic(parms, x = Day)
-  fit <- out
-  sum((Infected - fit)^2)
+# Genera las gráficas del modelo SIR y de la curva logística.
+plot_results <- function(fit_sir, fit_logistic, infected_dates, infected, population) {
+  fechas_modelo <- seq.Date(from = infected_dates[1], length.out = nrow(fit_sir$fit), by = "day")
+  fit_sir$fit$time <- fechas_modelo
+
+  plot(fit_sir$fit$time, fit_sir$fit$S, type = "l", lwd = 2, ylim = c(0, population))
+  lines(fit_sir$fit$time, fit_sir$fit$I, lwd = 2, col = "red")
+  lines(fit_sir$fit$time, fit_sir$fit$R, lwd = 2, col = "green")
+  points(infected_dates, infected, pch = 19)
+  grid()
+  legend("left", c("Susceptibles", "Infectados", "Recobrados"), lty = 1, lwd = 2, col = 1:3, inset = 0.05)
+
+  plot(fit_sir$fit$time, fit_sir$fit$I, type = "l", lwd = 2, col = "red", pch = 19)
+  points(infected_dates, infected, pch = 19)
+  grid()
+
+  plot(infected_dates, fit_logistic$fit, type = "l", lwd = 2, col = "blue",
+       main = "Curva logística ajustada", ylab = "Casos", xlab = "Fecha")
+  points(infected_dates, infected, pch = 19, col = "red")
+  grid()
 }
 
-Opt <- optim(c(1, 1, 2e6), RSS, method = "L-BFGS-B") 
+# Ejecuta el flujo completo: descarga datos, ajusta modelos y devuelve un resumen.
+run_analysis <- function(country = "Spain", population = 47e6, min_cases = 100, horizon_days = 90) {
+  data <- load_case_data()
+  prepared <- prepare_country_series(data, country, min_cases)
+  growth <- compute_growth_metrics(prepared$infected)
 
-fit <- logistic(Opt$par, 1:length(Fechas))
-plot(as.Date(Fechas), fit)
-points(Fechas_Obs, Infected, pch = 19, col = "red")
-grid()
+  sir <- fit_sir_model(prepared$infected, population, horizon_days)
+  r0 <- sir$parameters["beta"] / sir$parameters["gamma"]
+  logistic <- fit_logistic_curve(prepared$infected, prepared$series$date)
 
+  plot_results(sir, logistic, prepared$series$date, prepared$infected, population)
 
+  list(
+    country = country,
+    start_date = prepared$start_date,
+    sir_parameters = sir$parameters,
+    r0 = r0,
+    logistic_parameters = logistic$parameters,
+    growth = growth
+  )
+}
 
-
-
-
-
-Fechas <- seq.Date(from = Fecha, length.out = length(Infected), by = "day")
-points(Fechas, Infected)
-legend("right", c("Susceptibles", "Infecteds", "Recovereds"), lty = 1, lwd = 2, col = col, inset = 0.05)
-title("SIR model 2019-nCoV Espa?a", outer = TRUE, line = -2)
+if (sys.nframe() == 0) {
+  results <- run_analysis()
+  print(results$sir_parameters)
+  message(sprintf("R0 estimado: %.2f", results$r0))
+}
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # SIR
-modelo epidemiológico SIR
+
+Modelo epidemiológico SIR aplicado a la serie de casos confirmados de COVID-19.
+
+## Requisitos
+El script depende de los siguientes paquetes de R:
+- dplyr
+- lubridate
+- tidyr
+- reshape2
+- xts
+- zoo
+- deSolve
+
+Instala las dependencias desde una sesión de R si no están disponibles:
+
+```r
+install.packages(c("dplyr", "lubridate", "tidyr", "reshape2", "xts", "zoo", "deSolve"))
+```
+
+## Ejecución
+El análisis principal se encuentra en `R/modelo.R` y descarga automáticamente la
+serie de casos global desde el repositorio público de Johns Hopkins. Para
+lanzarlo con los valores predeterminados (España, umbral de 100 casos y 90 días
+de horizonte), ejecuta:
+
+```bash
+Rscript R/modelo.R
+```
+
+El script ajusta un modelo SIR y una curva logística básica, calcula el número
+reproductivo básico estimado (`R0`) y genera gráficos de las curvas ajustadas.
+
+## Parámetros
+Puedes modificar el país, la población total, el umbral mínimo de casos o el
+horizonte de proyección invocando la función `run_analysis()` desde R, por
+ ejemplo:
+
+```r
+source("R/modelo.R")
+resultados <- run_analysis(country = "Italy", population = 60e6, min_cases = 200)
+```
