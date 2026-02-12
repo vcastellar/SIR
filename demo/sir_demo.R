1d872b5eaac8e753c7dfcaa26440a4a5142aeca0
vcastellar <vic.sebastia@gmail.com>
2026-02-12 19:09:30 +0100

    demo registry

--- demo/sir_demo.R
+++ demo/sir_demo.R
@@ -1,102 +1,43 @@
-## Demo: Simulación y visualización de un modelo SIR
-##
-## Este demo muestra cómo:
-## 1) simular una epidemia SIR con simulate_sir()
-## 2) integrar explícitamente el modelo SIR con sir_c() usando deSolve
-## 3) visualizar infectados activos e incidencia observada
+sird_rhs <- function(time, state, parms) {
+  with(as.list(c(state, parms)), {
+    N <- S + I + R
+    lambda <- beta * S * I / N
 
-## Cargar paquetes necesarios
-library(deSolve)
+    dS <- -lambda
+    dI <-  lambda - gamma * I - mu * I
+    dR <-  gamma * I
+    dD <-  mu * I
 
-## Mensaje inicial
-cat("Demo SIR: simulación e integración del modelo\n")
-cat("=============================================\n\n")
+    list(
+      c(dS, dI, dR, dD),
+      incidence = lambda
+    )
+  })
+}
 
-## ------------------------------------------------------------
-## 1) Simular datos con simulate_sir()
-## ------------------------------------------------------------
-cat("1) Simulando datos con simulate_sir()...\n")
-
-sim <- simulate_sir(
-  n_days = 300,
-  N = 1e6,
-  beta = 0.30,
-  gamma = 0.10,
-  I0 = 20,
-  rho = 0.25,
-  obs = "negbin",
-  size = 15,
-  seed = 1
+SIRD_MODEL <- epi_model(
+  name      = "SIRD",
+  rhs       = sird_rhs,
+  states    = c("S", "I", "R", "D"),
+  flows     = c("incidence"),
+  par_names = c("beta", "gamma", "mu"),
+  defaults  = c(beta = 0.3, gamma = 0.1, mu = 0.01),
+  init      = c(S = 1e6, I = 10, R = 0, D = 0),
+  roles     = list(susceptible = "S",
+                   infectious  = "I",
+                   recovered   = "R",
+                   deceased    = "D"
+                   )
 )
 
-cat("   Simulación completada.\n\n")
+register_epi_model(SIRD_MODEL, family = 'SIRD')
+run_epi_app()
 
-## Inspección rápida
-cat("Primeras filas de la incidencia observada:\n")
-print(head(sim$incidence_obs))
+sim <- simulate_epi(model = SIRD_MODEL,
+                    times = 0:200,
+                    parms = SIRD_MODEL$defaults,
+                    init =  SIRD_MODEL$init)
+plot(sim)
 
-cat("\nPrimeras filas de los casos acumulados observados:\n")
-print(head(sim$cumulative_obs))
-
-## ------------------------------------------------------------
-## 2) Integrar explícitamente el modelo SIR con sir_c()
-## ------------------------------------------------------------
-cat("\n2) Integrando el modelo SIR con sir_c()...\n")
-
-times <- sim$states$time
-
-init <- c(
-  S = sim$params$N - sim$params$I0,
-  I = sim$params$I0,
-  R = 0,
-  C = sim$params$I0
-)
-
-pars <- c(
-  beta = sim$params$beta,
-  gamma = sim$params$gamma
-)
-
-out <- ode(
-  y = init,
-  times = times,
-  func = sir_c,
-  parms = pars,
-  method = "lsoda"
-)
-
-out <- as.data.frame(out)
-
-cat("   Integración completada.\n\n")
-
-## ------------------------------------------------------------
-## 3) Gráficos
-## ------------------------------------------------------------
-cat("3) Mostrando gráficos...\n")
-
-op <- par(no.readonly = TRUE)
-par(mfrow = c(2, 1), mar = c(4, 4, 2, 1))
-
-## Infectados activos
-plot(
-  out$time, out$I,
-  type = "l",
-  xlab = "Días",
-  ylab = "I(t)",
-  main = "SIR: Infectados activos"
-)
-
-## Incidencia observada
-plot(
-  sim$incidence_obs$time,
-  sim$incidence_obs$inc,
-  type = "h",
-  xlab = "Días",
-  ylab = "Incidencia observada",
-  main = "Incidencia observada (conteos)"
-)
-
-par(op)
-
-cat("\nDemo finalizada.\n")
-cat("Puedes volver a ejecutarla con: demo('sir_demo')\n")
+plot(instantaneous_growth_rate(sim), type = 'l')
+plot(doubling_time_ts(sim), type = 'l')
