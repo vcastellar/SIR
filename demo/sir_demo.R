32a3e11c46cc841e95f8ceff24d31bddf9effe46
vcastellar <vic.sebastia@gmail.com>
2026-02-14 17:00:17 +0100

    restructuracion

--- demo/sir_demo.R
+++ demo/sir_demo.R
@@ -1,38 +1,23 @@
-sird_rhs <- function(time, state, parms) {
+sirv_rhs <- function(time, state, parms) {
+
   with(as.list(c(state, parms)), {
-    N <- S + I + R
-    lambda <- beta * S * I / N
 
-    dS <- -lambda
-    dI <-  lambda - gamma * I - mu * I
+    dS <- -beta * S * I - nu * S
+    dI <-  beta * S * I - gamma * I
     dR <-  gamma * I
-    dD <-  mu * I
+    dV <-  nu * S
 
-    list(
-      c(dS, dI, dR, dD),
-      incidence = lambda
-    )
+    list(c(dS, dI, dR, dV))
   })
 }
 
-SIRD_MODEL <- epi_model(
-  name      = "SIRD",
-  rhs       = sird_rhs,
-  states    = c("S", "I", "R", "D"),
-  flows     = c("incidence"),
-  par_names = c("beta", "gamma", "mu"),
-  defaults  = c(beta = 0.3, gamma = 0.1, mu = 0.01),
-  init      = c(S = 1e6, I = 10, R = 0, D = 0)
+
+SIRV_MODEL <- epi_model(
+  name      = "SIRV",
+  rhs       = sirv_rhs,
+  par_names = c("beta", "gamma", "nu"),
+  states    = c("S", "I", "R", "V")
 )
 
-register_epi_model(SIRD_MODEL, family = 'SIRD')
-run_epi_app()
 
-sim <- simulate_epi(model = SIRD_MODEL,
-                    times = 0:200,
-                    parms = SIRD_MODEL$defaults,
-                    init =  SIRD_MODEL$init)
-plot(sim)
-
-plot(instantaneous_growth_rate(sim), type = 'l')
-plot(doubling_time_ts(sim), type = 'l')
+register_epi_model(SIRV_MODEL, family = "SIR")
