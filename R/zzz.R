70f5e6a013bee0003e3c725bc95599eea7eba061
vcastellar <vic.sebastia@gmail.com>
2026-02-15 09:40:23 +0100

    eliminación de registry

--- R/zzz.R
+++ R/zzz.R
@@ -1,25 +1,13 @@
 #' @importFrom stats coef setNames
 #' @importFrom graphics plot.new text
 #' @importFrom shiny observeEvent
-#' @importFrom registry registry
 
 .onLoad <- function(libname, pkgname) {
 
-  register_epi_model(SI_MODEL, family = "SI")
-  register_epi_model(SIR_MODEL, family = "SIR")
-  register_epi_model(SIRS_MODEL, family = "SIRS")
-  register_epi_model(SEIR_MODEL, family = "SEIR")
-  register_epi_model(SEIRS_MODEL, family = "SEIRS")
-  register_epi_model(SIR_VITAL_MODEL, family = "SIR_VITAL")
+  register_epi_model(SI_MODEL)
+  register_epi_model(SIR_MODEL)
+  register_epi_model(SIRS_MODEL)
+  register_epi_model(SEIR_MODEL)
+  register_epi_model(SEIRS_MODEL)
+  register_epi_model(SIR_VITAL_MODEL)
 }
-
-.register_builtin <- function(model, family) {
-
-  epi_registry$set_entry(
-    name = model$name,
-    model = model,
-    family = family,
-    origin = "builtin"
-  )
-}
-
